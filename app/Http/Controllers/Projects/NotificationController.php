8c46295040b1216c61bfe9500158561e6ff998d5
philip@murowcm.com <philip@murowcm.com>
2017-11-13 15:57:39 +0530

    notification

--- app/Http/Controllers/Projects/NotificationController.php
+++ app/Http/Controllers/Projects/NotificationController.php
@@ -324,55 +324,55 @@ class NotificationController extends Controller {
         /*--------------------------------------Pre--Bidding----------------------------------------------------*/
         
         
-        $bids = DB::table('project_bid_documents')
-        //->leftJoin('project_firm as lead_agency', 'project_bid_documents.bd_lead_agency', '=', 'lead_agency.f_id')
-        //->leftJoin('projects as type_of_improvement', 'project_bid_documents.bd_type_of_improvement', '=', 'type_of_improvement.p_id')
-        // ->leftJoin('documents as upload_addendum_path', 'project_bid_documents.bd_upload_addendum_path', '=', 'upload_addendum_path.doc_id')
-        //->leftJoin('documents as addvertisement_bid_path', 'project_bid_documents.bd_addvertisement_of_bid_path', '=', 'addvertisement_bid_path.doc_id')
-        //->leftJoin('documents as detail_result_path', 'project_bid_documents.bd_detail_result_path', '=', 'detail_result_path.doc_id')
-        //->leftJoin('documents as notice_invite_bid_path', 'project_bid_documents.bd_notice_invite_bid_path', '=', 'notice_invite_bid_path.doc_id')
-        //->leftJoin('project_firm as low_bidder_name', 'project_bid_documents.bd_low_bidder_name', '=', 'low_bidder_name.f_id')
-        //->leftJoin('documents as sucessful_bidder_proposal_path', 'project_bid_documents.bd_sucessful_bidder_proposal_path', '=', 'sucessful_bidder_proposal_path.doc_id')
-        ->leftJoin('projects', 'project_bid_documents.bd_project_id', '=', 'projects.p_id')
-        ->leftJoin('users', 'project_bid_documents.bd_user_id', '=', 'users.id')
-        ->select('project_bid_documents.*', 'project_bid_documents.bd_id as bd_id', 'project_bid_documents.bd_status as bd_status',
-        //'lead_agency.f_name as lead_agency', 
-        //'type_of_improvement.p_description as type_of_improvement', 
-        // 'upload_addendum_path.doc_path as upload_addendum_path', 
-        //'addvertisement_bid_path.doc_path as addvertisement_bid_path', 
-        //'detail_result_path.doc_path as detail_result_path', 
-        //'notice_invite_bid_path.doc_path as notice_invite_bid_path', 
-        //'low_bidder_name.f_name as low_bidder_name',
-        //'sucessful_bidder_proposal_path.doc_path as sucessful_bidder_proposal_path', 
-        'projects.*',
-        'users.username', 'users.email', 'users.first_name', 'users.last_name as user_lastname', 'users.company_name as user_company', 'users.phone_number as user_phonenumber', 'users.status as user_status', 'users.role as user_role')
-        ->whereRaw('date(project_bid_documents.bd_date_of_opening) = ?',[date('Y-m-d')])
-        ->get();
-        foreach($bids as $check_project_user)
-        {
-            $project_id           = $check_project_user->p_id;
-            $notification_title   = 'Bid for ' .$check_project_user->p_name.' Project starts today.';
-            $url                  = App::make('url')->to('/'); 
-            $link                 = "/dashboard/".$project_id."/bid_documents/".$check_project_user->bd_id;
-            $date                 = date("M d, Y h:i a");
-            $email_description    = 'The Bid for <strong>'.$check_project_user->p_name.'</strong> Project starts today. <a href="'.$url.$link.'"> Click Here to see </a>';
-            $user_detail = array(
-              'id'              => $check_project_user->bd_id,
-              'name'            => $check_project_user->username,
-              'email'           => $check_project_user->email,
-              'link'            => $link,
-              'date'            => $date,
-              'project_name'    => $check_project_user->p_name,
-              'title'           => $notification_title,
-              'description'     => $email_description
-            );
-            $user_single = (object) $user_detail;
-            Mail::send('emails.send_notification',['user' => $user_single], function ($message) use ($user_single) {
-                $message->from('no-reply@sw.ai', 'StratusCM');
-                $message->to($user_single->email, $user_single->name)->subject($user_single->title);
-            }); 
-        }
-        
+//        $bids = DB::table('project_bid_documents')
+//        //->leftJoin('project_firm as lead_agency', 'project_bid_documents.bd_lead_agency', '=', 'lead_agency.f_id')
+//        //->leftJoin('projects as type_of_improvement', 'project_bid_documents.bd_type_of_improvement', '=', 'type_of_improvement.p_id')
+//        // ->leftJoin('documents as upload_addendum_path', 'project_bid_documents.bd_upload_addendum_path', '=', 'upload_addendum_path.doc_id')
+//        //->leftJoin('documents as addvertisement_bid_path', 'project_bid_documents.bd_addvertisement_of_bid_path', '=', 'addvertisement_bid_path.doc_id')
+//        //->leftJoin('documents as detail_result_path', 'project_bid_documents.bd_detail_result_path', '=', 'detail_result_path.doc_id')
+//        //->leftJoin('documents as notice_invite_bid_path', 'project_bid_documents.bd_notice_invite_bid_path', '=', 'notice_invite_bid_path.doc_id')
+//        //->leftJoin('project_firm as low_bidder_name', 'project_bid_documents.bd_low_bidder_name', '=', 'low_bidder_name.f_id')
+//        //->leftJoin('documents as sucessful_bidder_proposal_path', 'project_bid_documents.bd_sucessful_bidder_proposal_path', '=', 'sucessful_bidder_proposal_path.doc_id')
+//        ->leftJoin('projects', 'project_bid_documents.bd_project_id', '=', 'projects.p_id')
+//        ->leftJoin('users', 'project_bid_documents.bd_user_id', '=', 'users.id')
+//        ->select('project_bid_documents.*', 'project_bid_documents.bd_id as bd_id', 'project_bid_documents.bd_status as bd_status',
+//        //'lead_agency.f_name as lead_agency', 
+//        //'type_of_improvement.p_description as type_of_improvement', 
+//        // 'upload_addendum_path.doc_path as upload_addendum_path', 
+//        //'addvertisement_bid_path.doc_path as addvertisement_bid_path', 
+//        //'detail_result_path.doc_path as detail_result_path', 
+//        //'notice_invite_bid_path.doc_path as notice_invite_bid_path', 
+//        //'low_bidder_name.f_name as low_bidder_name',
+//        //'sucessful_bidder_proposal_path.doc_path as sucessful_bidder_proposal_path', 
+//        'projects.*',
+//        'users.username', 'users.email', 'users.first_name', 'users.last_name as user_lastname', 'users.company_name as user_company', 'users.phone_number as user_phonenumber', 'users.status as user_status', 'users.role as user_role')
+//        ->whereRaw('date(project_bid_documents.bd_date_of_opening) = ?',[date('Y-m-d')])
+//        ->get();
+//        foreach($bids as $check_project_user)
+//        {
+//            $project_id           = $check_project_user->p_id;
+//            $notification_title   = 'Bid for ' .$check_project_user->p_name.' Project starts today.';
+//            $url                  = App::make('url')->to('/'); 
+//            $link                 = "/dashboard/".$project_id."/bid_documents/".$check_project_user->bd_id;
+//            $date                 = date("M d, Y h:i a");
+//            $email_description    = 'The Bid for <strong>'.$check_project_user->p_name.'</strong> Project starts today. <a href="'.$url.$link.'"> Click Here to see </a>';
+//            $user_detail = array(
+//              'id'              => $check_project_user->bd_id,
+//              'name'            => $check_project_user->username,
+//              'email'           => $check_project_user->email,
+//              'link'            => $link,
+//              'date'            => $date,
+//              'project_name'    => $check_project_user->p_name,
+//              'title'           => $notification_title,
+//              'description'     => $email_description
+//            );
+//            $user_single = (object) $user_detail;
+//            Mail::send('emails.send_notification',['user' => $user_single], function ($message) use ($user_single) {
+//                $message->from('no-reply@sw.ai', 'StratusCM');
+//                $message->to($user_single->email, $user_single->name)->subject($user_single->title);
+//            }); 
+//        }
+//        
         /*------------------------------During Construction-------RFIs------------------------------------------*/
         
         /*------------------------------During Construction-------Submittals EOR Approval-----------------------*/
@@ -446,12 +446,12 @@ class NotificationController extends Controller {
         
         
         
-        //echo '<pre>';print_r($bids);die;
+        echo '<pre>';print_r($meetings);die;
         
         
         
         
         //print_r($request);
-        //die;
+        die;
     }
 }
