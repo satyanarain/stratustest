072bc7ff6f431d918e65cc3712ce763ad010f428
philip@murowcm.com <philip@murowcm.com>
2018-01-22 12:25:53 +0530

    fixes 22-01

--- resources/views/survey/survey_add.blade.php
+++ resources/views/survey/survey_add.blade.php
@@ -205,22 +205,19 @@
            <tr><td colspan="5">&nbsp;</td></tr>
            
            <tr>
-            <td rowspan="4" valign="top"><b>To:</b></td>
+            <td rowspan="3" valign="top"><b>To:</b></td>
             <td colspan="4"><b>SURVEY COORDINATOR:</b></td>
           </tr>
-            
             <tr>
-              <td style="border-bottom:1px solid #000;" align="center">&nbsp;</td>
+              <td style="border-bottom:1px solid #000;" align="center">Task No.</td>
+              <td style="border-bottom:1px solid #000;" colspan="3"><b>DESCRIPTION OF SURVEY REQUIRED: (Please prioritize & provide details)</b></td>
+
+            </tr>
+            <tr>
+              <td style="border-bottom:1px solid #000;" align="center">1</td>
               <td style="border-bottom:1px solid #000;" colspan="3">&nbsp;</td>
             </tr> 
-            <tr>
-              <td style="border-bottom:1px solid #000;" align="center">&nbsp;</td>
-              <td style="border-bottom:1px solid #000;" colspan="3">&nbsp;</td>
-            </tr> 
-            <tr>
-              <td style="border-bottom:1px solid #000;" align="center">&nbsp;</td>
-              <td style="border-bottom:1px solid #000;" colspan="3">&nbsp;</td>
-            </tr> 
+             
              
            <tr><td colspan="5" style="border-bottom:2px solid #000;">&nbsp;</td></tr>
            
