613063020df58959625c408c6e634eb33dd7e0e1
philip@murowcm.com <philip@murowcm.com>
2018-02-07 10:26:02 +0530

    phase3 changes 06-02-2018

--- resources/views/bid_documents/bid_documents_add.blade.php
+++ resources/views/bid_documents/bid_documents_add.blade.php
@@ -41,7 +41,7 @@
                 </div>
                 <span id="project_type"></span><br/>
                 <span class="sub-title">Select improvement type if you want to change</span>
-                <button class="label label-warning add-impvtypes" style="margin-bottom: 5px;">Add New Improvement Type</button>
+<!--                <button class="label label-warning add-impvtypes" style="margin-bottom: 5px;">Add New Improvement Type</button>-->
                 <div class="loading_data" style="text-align: center;">
                    <img src="{{ url('/resources/assets/img/loading_bar.gif') }}" alt=""/>
                 </div>
--- resources/views/notice_completion/notice_completion_add.blade.php
+++ resources/views/notice_completion/notice_completion_add.blade.php
@@ -399,7 +399,7 @@
                                     </div>
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="project_type">Improvement Type <span class="text-danger">*</span></label><button class="label label-warning add-impvtypes" style="margin-bottom: 5px;">Add New Improvement Type</button>
+<!--                                <label for="project_type">Improvement Type <span class="text-danger">*</span></label><button class="label label-warning add-impvtypes" style="margin-bottom: 5px;">Add New Improvement Type</button>-->
                                 <div id="project_type_selected" style=""></div>
                                 <select class="form-control project_type_dropdown" id="project_type_dropdown" placeholder="Select Improvement Types">
                                     <option>Select Improvement Types</option>
