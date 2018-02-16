6d83e950620367871c4617eed964f04cf8b3123d
philip@murowcm.com <philip@murowcm.com>
2018-02-16 16:59:49 +0530

    phase 3 changes 16-02

--- resources/assets/dist/submittals_review_view_all.js
+++ resources/assets/dist/submittals_review_view_all.js
@@ -154,6 +154,11 @@
 			           update_permission
 			       	]).draw( false );
 			  	}
+                            t.on( 'order.dt search.dt', function () {
+                                t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+                                    cell.innerHTML = i+1;
+                                } );
+                            } ).draw();
 			});
 		    // $( "h2" ).appendTo( $( ".container" ) );
 		   
