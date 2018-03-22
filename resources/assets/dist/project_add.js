1e572164c1781e80b2f6734a9263f693a89024d6
philip@murowcm.com <philip@murowcm.com>
2018-03-22 18:44:50 +0530

    phase 4 changes 22-03-2018

--- resources/assets/dist/project_add.js
+++ resources/assets/dist/project_add.js
@@ -221,7 +221,7 @@ $(document).ready(function() {
                 $("#project_wage_determination").removeAttr('value');
                 $('#project_terms').attr('checked', false);
                 $("#project_lead_agency").removeAttr('value');
-                $("#project_location").removeAttr('value');
+                //$("#project_location").removeAttr('value');
                 $("#project_description").removeAttr('value');
                 $(".select2-search-choice-close").trigger("click");
                 $("#project_type option:selected").removeAttr("selected");
