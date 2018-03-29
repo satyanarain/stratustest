7d909d460be992d2082ef4f1476d00210c5ff0f7
philip@murowcm.com <philip@murowcm.com>
2018-03-29 14:12:03 +0530

    docusign integration 29-03-2018

--- app/Http/Controllers/Projects/NoticeAwardController.php
+++ app/Http/Controllers/Projects/NoticeAwardController.php
@@ -193,7 +193,7 @@ class NoticeAwardController extends Controller {
             $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
             if ( $status != 201 ) {
                    // $response = json_decode($json_response, true);
-                    return response()->json(['error' => "Error calling DocuSign.".print_r($json_response['message'])], 500);
+                    return response()->json(['error' => "Error calling DocuSign.".print_r($json_response)], 500);
 //                    echo "Error calling DocuSign." . $status . "\nerror text: ";
 //                    print_r($json_response['message']); echo "\n";
 //                    exit(-1);
