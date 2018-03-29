cfd6c973660a7a76c15cd62ed9301a9be29d5295
philip@murowcm.com <philip@murowcm.com>
2018-03-29 15:06:34 +0530

    docusign integration 29-03-2018

--- app/ProjectNoticeofAward.php
+++ app/ProjectNoticeofAward.php
@@ -13,7 +13,7 @@ class ProjectNoticeofAward extends Authenticatable
     protected $table = 'project_notice_award';
 
     protected $fillable = [
-        'pna_id', 'pna_notice_status', 'pna_improvement_type', 'pna_contactor_name', 'pna_currency', 'pna_contact_amount', 'pna_award_date', 'pna_notice_path', 'pna_notice_sign', 'pna_project_id', 'pna_user_id', 'pna_status', 'pna_timestamp'
+        'pna_envelope_id','pna_docusign_status','pna_id', 'pna_notice_status', 'pna_improvement_type', 'pna_contactor_name', 'pna_currency', 'pna_contact_amount', 'pna_award_date', 'pna_notice_path', 'pna_notice_sign', 'pna_project_id', 'pna_user_id', 'pna_status', 'pna_timestamp'
     ];
 
 
