df78bc00f82de5f9797061eb89ef9c3c88606414
philip@murowcm.com <philip@murowcm.com>
2018-03-29 16:53:08 +0530

    docusign integration 29-03-2018

--- resources/assets/dist/notice_award_add.js
+++ resources/assets/dist/notice_award_add.js
@@ -526,9 +526,6 @@ $(document).ready(function() {
                 html += '<li>Signatory email is invalid.</li>';
                 is_error = true;
             }
-            alert($(this).val());
-//            if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($(this).val()))
-//                signatory_email.push($(this).val());
         });
         var item = {};
         item['signatory_name'] 		= signatory_name;
