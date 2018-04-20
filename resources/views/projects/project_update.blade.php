90c9575c953d7c6fa43e126ea831299ed3a9b066
pankajmonga <pankaj.monga@live.com>
2018-04-20 19:22:21 +0530

    add improvement option in update page

--- resources/views/projects/project_update.blade.php
+++ resources/views/projects/project_update.blade.php
@@ -786,20 +786,12 @@ function initMap1() {
 
 
   var map = new google.maps.Map(document.getElementById('map1'), {
-            center: {lat: lat, lng: long},
-            mapTypeId: google.maps.MapTypeId.TERRAIN,
-            zoom: 7,
-            scrollwheel: false
+          center: {lat: 33.7174708, lng: -117.83114280000001},
+          zoom: 11,
+          scrollwheel: false
         });
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, long),
-            map: map,
-            label: document.getElementById('project_location').value
-        });
-        marker.setVisible(true);
-
         var input = /** @type {!HTMLInputElement} */(
-            document.getElementById('project_location'));
+            document.getElementById('firm_address'));
 
         var types = document.getElementById('type-selector');
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -810,50 +802,48 @@ function initMap1() {
 
         var infowindow = new google.maps.InfoWindow();
         var marker = new google.maps.Marker({
-            map: map,
-            anchorPoint: new google.maps.Point(0, -29)
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
         });
 
         autocomplete.addListener('place_changed', function() {
-            var place = autocomplete.getPlace();
-            if (!place.geometry) {
-                window.alert("Autocomplete's returned place contains no geometry");
-                return;
-            }
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+          }
 
-            // If the place has a geometry, then present it on a map.
-            if (place.geometry.viewport) {
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);  // Why 17? Because it looks good.
-            }
-            marker.setIcon(/** @type {google.maps.Icon} */({
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(35, 35)
-            }));
-            marker.setPosition(place.geometry.location);
-            marker.setVisible(true);
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
 
-            var address = '';
-            if (place.address_components) {
-                address = [
-                    (place.address_components[0] && place.address_components[0].short_name || ''),
-                    (place.address_components[1] && place.address_components[1].short_name || ''),
-                    (place.address_components[2] && place.address_components[2].short_name || '')
-                ].join(' ');
-            }
-
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
             $("#project_latitude").val(place.geometry.location.lat());
-            $("#project_longitude").val(place.geometry.location.lng())
-
-
-
-            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-            infowindow.open(map, marker);
+            $("#project_longitude").val(place.geometry.location.lng());
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map, marker);
         });
 
 
