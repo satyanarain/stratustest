96ebd54685e15981998de5426ac08eb8dd6d58bd
pankajmonga <pankaj.monga@live.com>
2018-04-20 18:42:08 +0530

    add improvement option in update page

--- resources/views/projects/project_update.blade.php
+++ resources/views/projects/project_update.blade.php
@@ -347,7 +347,7 @@
 <!-- Placed js at the end of the document so the pages load faster -->
 <script src="{{ url('/resources/assets/js/jquery-1.10.2.min.js') }}"></script>
 <script src="{{ url('/resources/assets/dist/api_url.js') }}"></script>
-
+<script src="{{ url('/resources/assets/dist/project_update.js') }}"></script>
 <script>
 $(document).ready(function() {
     var curr_url = $(location).attr('href')
@@ -654,7 +654,8 @@ $(document).ready(function() {
         
     })
   })  
-  function initMap1() {
+
+function initMap1() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 33.7174708, lng: -117.83114280000001},
           zoom: 11,
@@ -715,10 +716,74 @@ $(document).ready(function() {
           infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
           infowindow.open(map, marker);
         });
+        
+        
+            
+        var map1 = new google.maps.Map(document.getElementById('map1'), {
+          center: {lat: 33.7174708, lng: -117.83114280000001},
+          zoom: 11,
+          scrollwheel: false
+        });
+        var input = /** @type {!HTMLInputElement} */(
+            document.getElementById('project_location'));
+
+        var types = document.getElementById('type-selector');
+        map1.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        map1.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map1);
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map1: map1,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map1.fitBounds(place.geometry.viewport);
+          } else {
+            map1.setCenter(place.geometry.location);
+            map1.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+            $("#project_latitude").val(place.geometry.location.lat());
+            $("#project_longitude").val(place.geometry.location.lng());
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map1, marker);
+          return false;
+
+        });
       }
 </script>
 
-<script src="{{ url('/resources/assets/dist/project_update.js') }}"></script>
+
 
 
 <!-- <script src="{{ url('/resources/assets/dist/project_add.js') }}"></script> -->
@@ -886,6 +951,17 @@ $(document).ready(function() {
     }
   });
 
+        $('#project_lead_agency').change(function(){
+        var company = $(this).val();
+        if(company=="Add New Agency")
+        {
+            $('#add-agency').modal('show');
+            $('#add-agency').on('shown.bs.modal',function(){
+                google.maps.event.trigger(map, "resize");
+                google.maps.event.trigger(map1, "resize");
+              });
+        }
+    });
 </script>
 
 @include('include/footer')
