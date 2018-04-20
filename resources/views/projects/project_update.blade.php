c927f603bdaea6c3cc39a2f7a3835f080fb1552b
pankajmonga <pankaj.monga@live.com>
2018-04-20 19:17:41 +0530

    add improvement option in update page

--- resources/views/projects/project_update.blade.php
+++ resources/views/projects/project_update.blade.php
@@ -229,11 +229,11 @@
 <!-- page head end-->
 <style>
   #map1 {
-    height: 200px;
-   /* position: relative !important;
-        overflow: inherit !important;*/
-        margin-top: 40px !important;
-        overflow:visible !important;
+    height: 300px;
+     /* position: relative !important;
+      overflow: inherit !important;*/
+      margin-top: 20px !important;
+      overflow:visible !important;
   }
 
     .gm-style {
@@ -785,13 +785,21 @@ function initMap1() {
         });
 
 
-          var map = new google.maps.Map(document.getElementById('map1'), {
-          center: {lat: 33.7174708, lng: -117.83114280000001},
-          zoom: 11,
-          scrollwheel: false
+  var map = new google.maps.Map(document.getElementById('map1'), {
+            center: {lat: lat, lng: long},
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            zoom: 7,
+            scrollwheel: false
         });
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, long),
+            map: map,
+            label: document.getElementById('project_location').value
+        });
+        marker.setVisible(true);
+
         var input = /** @type {!HTMLInputElement} */(
-            document.getElementById('firm_address'));
+            document.getElementById('project_location'));
 
         var types = document.getElementById('type-selector');
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -802,113 +810,52 @@ function initMap1() {
 
         var infowindow = new google.maps.InfoWindow();
         var marker = new google.maps.Marker({
-          map: map,
-          anchorPoint: new google.maps.Point(0, -29)
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29)
         });
 
         autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            window.alert("Autocomplete's returned place contains no geometry");
-            return;
-          }
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                window.alert("Autocomplete's returned place contains no geometry");
+                return;
+            }
 
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-          }
-          marker.setIcon(/** @type {google.maps.Icon} */({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-          }));
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);  // Why 17? Because it looks good.
+            }
+            marker.setIcon(/** @type {google.maps.Icon} */({
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            }));
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
 
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
-            $("#project_latitude").val(place.geometry.location.lat());
-            $("#project_longitude").val(place.geometry.location.lng());
-          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-          infowindow.open(map, marker);
-        });
-        
-        
-            
-        var map1 = new google.maps.Map(document.getElementById('map1'), {
-          center: {lat: 33.7174708, lng: -117.83114280000001},
-          zoom: 11,
-          scrollwheel: false
-        });
-        var input = /** @type {!HTMLInputElement} */(
-            document.getElementById('project_location'));
-
-        var types = document.getElementById('type-selector');
-        map1.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-        map1.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-        var autocomplete = new google.maps.places.Autocomplete(input);
-        autocomplete.bindTo('bounds', map1);
-        var infowindow = new google.maps.InfoWindow();
-        var marker = new google.maps.Marker({
-          map1: map1,
-          anchorPoint: new google.maps.Point(0, -29)
-        });
-
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            window.alert("Autocomplete's returned place contains no geometry");
-            return;
-          }
-
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map1.fitBounds(place.geometry.viewport);
-          } else {
-            map1.setCenter(place.geometry.location);
-            map1.setZoom(17);  // Why 17? Because it looks good.
-          }
-          marker.setIcon(/** @type {google.maps.Icon} */({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-          }));
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
-
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
+            var address = '';
+            if (place.address_components) {
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+            }
 
             $("#project_latitude").val(place.geometry.location.lat());
-            $("#project_longitude").val(place.geometry.location.lng());
-          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-          infowindow.open(map1, marker);
-          return false;
+            $("#project_longitude").val(place.geometry.location.lng())
 
+
+
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+            infowindow.open(map, marker);
         });
+
 
 //        marker = new google.maps.Marker({
 //            position: new google.maps.LatLng(-33.8688, 151.2195),
