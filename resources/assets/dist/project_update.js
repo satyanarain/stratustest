09779aa54f465fcbec1c9c1b0c916f2fbe7cce1b
pankajmonga <pankaj.monga@live.com>
2018-04-24 19:09:12 +0530

    change 24-04-2018

--- resources/assets/dist/project_update.js
+++ resources/assets/dist/project_update.js
@@ -87,7 +87,7 @@
 		    cache: false
 		})
 		    .done(function(data, textStatus, jqXHR) {
-		    // console.log(data);
+		    console.log(data);
 		    var project_number = data.data.p_number;
 		    $('#project_number').val(project_number);
 		    var project_name = data.data.p_name;
@@ -190,7 +190,7 @@
         project_type = JSON.stringify(project_type);
         console.log(project_wage_determination);
        
-        jQuery.ajax({
+         jQuery.ajax({
             url: baseUrl + "projects/"+project_id+"/update",
             type: "POST",
             data: {
