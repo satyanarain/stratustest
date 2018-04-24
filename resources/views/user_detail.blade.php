8d60d8ed97372ca86f007f00f43556830fce57a9
pankajmonga <pankaj.monga@live.com>
2018-04-24 17:00:00 +0530

    change 24-04-2018

--- resources/views/user_detail.blade.php
+++ resources/views/user_detail.blade.php
@@ -60,10 +60,115 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+          <h4 class="modal-title">Updat User Profile </h4>
         </div>
         <div class="modal-body">
-          <p>Some text in the modal.</p>
+                         <div class="row">
+                    <div class="col-lg-12">
+                        <section class="panel">
+                            <div class="loading_data" style="text-align: center;">
+                                <img src="{{ url('/resources/assets/img/loading.svg') }}" alt="" />
+                            </div>
+                            <div class="panel-body">
+                            <div id="updateuserinfo" style="display: none"></div>
+                                    <div class="row">
+                                    <div class="form-group col-md-6 hide_user">
+                                        <label for="uname">Username <span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" id="uname" disabled>
+                                    </div>
+                                    <div class="form-group col-md-6 hide_user">
+                                        <label for="email">Email Address <span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" id="email" disabled>
+                                    </div>
+
+                                    <div class="form-group col-md-6">
+                                        <label for="fname">First Name</label>
+                                        <input type="text" class="form-control" id="fname">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="lname">Last Name</label>
+                                        <input type="text" class="form-control" id="lname">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="cname">Company Name</label>
+                                        <!-- <div class="loading_data" style="text-align: center;">
+                                           <img src="{{ url('/resources/assets/img/loading_bar.gif') }}" alt=""/>
+                                        </div> -->
+                                        <select class="form-control" id="firm_name">
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="position">Position / Title</label>
+                                        <input type="text" class="form-control" id="position">
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <!-- <div class="form-group col-md-6">
+                                        <label for="pnum">Phone Number</label>
+                                        <input type="text" class="form-control" id="pnum">
+                                    </div> -->
+                                    <div class="input_fields_wrap">
+                                    </div>
+
+                                    <div class="form-group col-md-6"></div>
+                                    <div class="clearfix"></div>
+
+                                    <div class="form-group col-md-6">
+                                        <label for="pass">Password</label>
+                                        <input type="password" class="form-control" id="pass">
+                                    </div>
+
+                                    <div class="form-group col-md-6">
+                                        <label for="cpass">Confirm Password</label>
+                                        <input type="password" class="form-control" id="cpass">
+                                    </div>
+
+
+                                    <input type="hidden" class="form-control role_normal_hide user_role" id="role">
+                                    <input type="hidden" class="form-control role_normal_hide user_status" id="status">
+                                    <div class="col-md-6 hide_user role_admin_hide">
+                                        <label>Status</label>
+                                        <select class="form-control user_status" id="status">
+                                            <option value="1">Verified</option>
+                                            <option value="0">Unverifed</option>
+                                            <option value="2">Suspended</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label for="standard_name">User Image <span class="text-danger">*</span><span id="old_image_link"></span><input type="hidden" id="old_image_path"></span> </label>
+                                        <form id="my-awesome-dropzone" action="{{ url('/document/uploadFiles') }}" class="dropzone">
+                                            <input type="hidden" name="document_path" value="/uploads/users/">
+                                        </form>
+
+                                        <div class="form-group">
+                                            <input type="hidden" name="user_image_path" id="user_image_path" value="">
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 hide_user role_admin_hide">
+                                        <!-- <label>User Type</label>
+                                        <select class="form-control user_role" id="role">
+                                            <option value="owner" class="owner_class">Owner</option>
+                                            <option value="manager">Manager</option>
+                                            <option value="contractor">Contractor</option>
+                                            <option value="engineer">Engineer</option>
+                                            <option value="surveyor">Surveyor</option>
+                                        </select> -->
+                                        <input type="hidden" class="form-control user_type_value" id="role">
+                                    </div>
+                                   <!--  <div class="col-md-6 hide_user">
+                                        <label>Project</label>
+                                        <div class="loading_data" style="text-align: center;">
+                                           <img src="{{ url('/resources/assets/img/loading_bar.gif') }}" alt=""/>
+                                        </div>
+                                        <select class="form-control" id="project_name">
+                                        </select>
+                                    </div> -->
+
+                                    </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
