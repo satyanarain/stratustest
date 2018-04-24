305eada48c55c99e3311a95fbf3f0e772d1a4954
pankajmonga <pankaj.monga@live.com>
2018-04-24 16:40:52 +0530

    change 24-04-2018

--- resources/views/user_detail.blade.php
+++ resources/views/user_detail.blade.php
@@ -1,17 +1,5 @@
 @include('include/header')
-<div id="profile_update" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">Profile Update </h3>
-    </div>
-    <div class="modal-body">
-        <p>modal body</p>
-    </div>
-    <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-        <button class="btn btn-primary">Save changes</button>
-    </div>
-</div>
+
        @include('include/sidebar')
 
        <!-- body content start-->
@@ -71,7 +59,19 @@
            </div>
            <!--body wrapper end-->
 
-         
+         <div id="profile_update" class="modal  fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="myModalLabel">Profile Update </h3>
+    </div>
+    <div class="modal-body">
+        <p>modal body</p>
+    </div>
+    <div class="modal-footer">
+        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+        <button class="btn btn-primary">Save changes</button>
+    </div>
+</div>
 
 <!-- Placed js at the end of the document so the pages load faster -->
 <script src="{{ url('/resources/assets/js/jquery-1.10.2.min.js') }}"></script>
