6c29540ec2a6e24587a675bb04edd6203f0427bf
pankajmonga <pankaj.monga@live.com>
2018-04-24 16:39:43 +0530

    change 24-04-2018

--- resources/views/user_detail.blade.php
+++ resources/views/user_detail.blade.php
@@ -1,4 +1,17 @@
 @include('include/header')
+<div id="profile_update" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h3 id="myModalLabel">Profile Update </h3>
+    </div>
+    <div class="modal-body">
+        <p>modal body</p>
+    </div>
+    <div class="modal-footer">
+        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+        <button class="btn btn-primary">Save changes</button>
+    </div>
+</div>
        @include('include/sidebar')
 
        <!-- body content start-->
@@ -71,16 +84,3 @@
 
 @include('include/footer')
 
-<div id="profile_update" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">Profile Update </h3>
-    </div>
-    <div class="modal-body">
-        <p>modal body</p>
-    </div>
-    <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-        <button class="btn btn-primary">Save changes</button>
-    </div>
-</div>
