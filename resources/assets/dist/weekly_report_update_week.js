4355a3a46738ee0f3d82260f2687921caa539901
pankajmonga <pankaj.monga@live.com>
2018-05-09 17:02:04 +0530

    add week report

--- resources/assets/dist/weekly_report_update_week.js
+++ resources/assets/dist/weekly_report_update_week.js
@@ -98,6 +98,9 @@ $(document).ready(function() {
         headers: {
           "x-access-token": token
         },
+         data: {
+            "type"           : "working_day",
+        },
         contentType: "application/json",
         cache: false
     })
@@ -230,8 +233,6 @@ $(document).ready(function() {
         jQuery.each( data.data, function( i, val ) {
             // console.log(val);
              var sign_date = new Date(val.update_time);
-
-             console.log(new Date(val.pwrd_date));
 
             document.getElementById('sign_date').innerHTML = formatDate(sign_date);
             $('#calendar_week_days').append(
