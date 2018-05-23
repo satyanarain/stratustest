f727f13c9ec487f096261053ca796418fe336f39
philip@murowcm.com <philip@murowcm.com>
2018-05-23 12:50:41 +0530

    change 23-05-2018

--- resources/assets/dist/weekly_report_update.js
+++ resources/assets/dist/weekly_report_update.js
@@ -374,7 +374,8 @@ $(document).ready(function() {
         {
             result.setDate(result.getDate() + total_days_plus);
             var revised_completion_date = $.datepicker.formatDate('yy-mm-dd', new Date(result));
-            //$('#revised_completion_date').text(revised_completion_date);
+            if(revised_completion_date)
+                $('#revised_completion_date').text(revised_completion_date);
         }else{
             $('#revised_completion_date').text('');
         }
