7b79eb0f1812fd9c5d28bbb28b201227855109df
philip@murowcm.com <philip@murowcm.com>
2018-06-11 12:45:11 +0530

    change 11-06-2018

--- resources/assets/dist/change_order_request_view_all.js
+++ resources/assets/dist/change_order_request_view_all.js
@@ -20,73 +20,6 @@ $(document).ready(function() {
         $('.body-content .wrapper').show();
     }
 
-    jQuery.ajax({
-        url: baseUrl + "projects/"+project_id,
-            type: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              "x-access-token": token
-            },
-            contentType: "application/json",
-            cache: false
-        })
-        .done(function(data, textStatus, jqXHR) {
-//            var project_id = data.data.p_id;
-//            $('#project_number').text(project_id);
-//            var project_number = data.data.p_number;
-//            $('#project_code').text(project_number);
-//            var project_name = data.data.p_name;
-//            $('#project_name').text(project_name);
-//            $('#project_name_1').text(project_name);
-//            var project_location = data.data.p_location;
-//            $('#project_location').text(project_location);
-//            $('#project_location1').text(project_location);
-//            var project_description = data.data.p_description;
-//            $('#project_description').text(project_description);
-
-            change_order_days_type = data.data.change_order_days_type;
-            change_order_due_date = data.data.change_order_due_date;
-            rfi_days_type =data.data.rfi_days_type;
-            rfi_due_date = data.data.rfi_due_date;
-            submittal_days_type =data.data.submittal_days_type;
-            submittal_due_date = data.data.submittal_due_date;
-
-//            if(data.data.f_name ==null)
-//                $('.project_lead_agency_li').remove();
-//            else
-//                $('#project_lead_agency').text(data.data.f_name);
-//
-//            var status = data.data.p_status;
-//            if(status == "active"){
-//                status = '<span class="label label-success">Active</span>';
-//            }
-//            else {
-//                status = '<span class="label label-danger">Inactive</span>';
-//            }
-//            $('#project_status').html(status);
-            $('.loading_project_detail').remove();
-        })
-        .fail(function(jqXHR, textStatus, errorThrown) 
-        {
-            console.log("HTTP Request Failed");
-            var response = jqXHR.responseJSON.code;
-            if(response == 403){
-                // window.location.href = baseUrl + "403";
-                console.log("403");
-                $('.loading_project_detail').remove();
-            }
-            else if(response == 404){
-                console.log("404");
-                $('.loading_project_detail').remove();
-                // window.location.href = baseUrl + "404";
-            }
-            else {
-                console.log("500");
-                $('.loading_project_detail').remove();
-                // window.location.href = baseUrl + "500";
-            }
-        })
-                
     // Get Project Currency
     jQuery.ajax({
     url: baseUrl+project_id+"/project_setting_get/project_currency",
@@ -377,11 +310,11 @@ $(document).ready(function() {
                 var oneDay = 24*60*60*1000;
                 var future_date = new Date(val.pcd_timestamp);
                 var futuredate = '';
-                if( change_order_days_type == 1 ) {
-                    futuredate = future_date.setDate(future_date.getDate() + change_order_due_date); 
+                if( val.change_order_days_type == 1 ) {
+                    futuredate = future_date.setDate(future_date.getDate() + val.change_order_due_date); 
                 }
                 else{
-                    futuredate = addWorkDays(change_order_due_date , (future_date));
+                    futuredate = addWorkDays(val.change_order_due_date , (future_date));
                     var updated_f = new Date(futuredate);
                     futuredate = updated_f.setDate(updated_f.getDate() + 0); 
                 }
